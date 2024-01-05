89770fe11344b15e709568772c251af8aaaeef30
Johandros <kochjohannes98@gmail.com>
2024-01-05 15:31:41 +0100

    config folder + manifest fix

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,15 @@ fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 game 'rdr3'
-author 'JSYS'
-repository ''
-description 'Doctor alert'
+author 'djaessel'
+repository 'https://github.com/djaessel/jsys_doctor_alert'
+description 'Doctor alert for RedM'
 
 lua54 'yes'
+
+shared_scripts {
+  'config/*.lua'
+}
 
 client_scripts {
   'client/*.lua',
