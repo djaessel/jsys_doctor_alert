4db67045ee4d30458fbf7b90ab3682f4c2383071
Johandros <kochjohannes98@gmail.com>
2024-01-08 20:10:11 +0100

    coords + range change

--- config/config.lua
+++ config/config.lua
@@ -3,14 +3,14 @@
 Config = {
     KeyBinding = 0xD51B784F, -- key hash - E / INPUT_CONTEXT_Y
     --KeyBinding = 0xCEFD9220, -- key hash - E
-    Range = 5, -- meters
+    Range = 2, -- meters
     Cooldown = 40, -- seconds
     TickerCheck = 500, -- milliseconds
     KeyInfoVisibleDuration = 5, -- seconds
 
     CoordinatesAll = {
         {
-            coords = vector3(10, 10, 1), -- x, y, z
+            coords = vector3(2726.69, -1231.85, 50.37), -- x, y, z
             command = "doctorsdalert"
         },
         {
