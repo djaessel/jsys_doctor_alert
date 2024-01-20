303382514d08de30453d8f18fafd65945809ad7b
Johandros <kochjohannes98@gmail.com>
2024-01-20 19:44:55 +0100

    config

--- config/config.lua
+++ config/config.lua
@@ -4,7 +4,7 @@ Config = {
     KeyBinding = 0xE30CD707, -- R
     Radius = 3, -- meters
     Cooldown = 60000, -- milliseconds
-    TickerCheck = 500, -- milliseconds
+    TickerCheck = 1000, -- milliseconds
     KeyInfoVisibleDuration = 5000, -- milliseconds
 
     CoordinatesAll = {
@@ -12,11 +12,11 @@ Config = {
             coords = vector3(2724.02, -1236.5, 49.95), -- x, y, z
             command = "doctorsdalert2"
         },
+        {
+            coords = vector3(1369.23, -1310.75, 77.94), -- x, y, z
+            command = "doctorrdalert2"
+        },
         -- below can be activated and changed, but unused at the moment!
---        {
---            coords = vector3(10, 20, 3), -- x, y, z
---            command = "doctorsdalert"
---        },
 --        {
 --            coords = vector3(40, 50, 6), -- x, y, z
 --            command = "doctorsdalert"
